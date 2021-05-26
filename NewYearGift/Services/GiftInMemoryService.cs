4ae5014ca4caf443ea1bc83636956fe37aa6c768
Kirill <kirill.kanabay@gmail.com>
2021-05-26 18:37:38 +0300

    Добавлен сервис подарков

--- NewYearGift/Services/GiftInMemoryService.cs
+++ NewYearGift/Services/GiftInMemoryService.cs
@@ -1,6 +1,28 @@
-﻿namespace NewYearGift.Services
+﻿using System.Collections.Generic;
+using NewYearGift.Models;
+
+namespace NewYearGift.Services
 {
-    class GiftInMemoryService
+    public class GiftInMemoryService
     {
+        private readonly List<Gift> _giftsList = new List<Gift>();
+        public List<Gift> GetAll() => _giftsList;
+        public Gift GetById(int id) => _giftsList[id];
+        public Gift Add(Gift gift)
+        {
+            _giftsList.Add(gift);
+            return gift;
+        }
+        public Gift Update(int id, Gift gift)
+        {
+            _giftsList[id] = gift;
+            return gift;
+        }
+        public Gift Delete(int id)
+        {
+            var deletedGift = _giftsList[id];
+            _giftsList.RemoveAt(id);
+            return deletedGift;
+        }
     }
 }
