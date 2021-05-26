ac00f89dc742cb40d568f7e6fc633ef2d8b31c08
Kirill <kirill.kanabay@gmail.com>
2021-05-26 14:05:05 +0300

    Добавлен класс подарков

---
+++ NewYearGift.BL/Gift.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace NewYearGift.BL
+{
+    /// <summary>
+    /// Класс подарка
+    /// </summary>
+    public class Gift
+    {
+        private string _name;
+        private readonly List<SweetBase> _sweets;
+
+        /// <summary>
+        /// Название подарка
+        /// </summary>
+        public string Name
+        {
+            get => _name;
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Название подарка не может быть пустым");
+                }
+
+                _name = Name;
+            }
+        }
+        /// <summary>
+        /// Количество конфет в подарке
+        /// </summary>
+        public int SweetsCount => _sweets.Count;
+        /// <summary>
+        /// Суммарный вес конфет
+        /// </summary>
+        public double TotalWeight => _sweets.Sum(s => s.Weight);
+        /// <summary>
+        /// Суммарная стоимость
+        /// </summary>
+        public decimal TotalPrice => _sweets.Sum(s => s.Price);
+        
+        public Gift(string name)
+        {
+            Name = name;
+            _sweets = new List<SweetBase>();
+        }
+
+        /// <summary>
+        /// Метод, добавляющий конфету в подарок
+        /// </summary>
+        /// <param name="sweet"></param>
+        public void AddSweet(SweetBase sweet)
+        {
+            if (sweet == null)
+            {
+                throw new ArgumentNullException(nameof(sweet), "Сладость не может быть Null");
+            }
+
+            _sweets.Add(sweet);
+        }
+
+        /// <summary>
+        /// Метод, удаляющий конфету из подарка
+        /// </summary>
+        /// <param name="sweetId"></param>
+        public void RemoveSweet(int sweetId)
+        {
+            if (sweetId < 0 || sweetId >= _sweets.Count)
+            {
+                throw new ArgumentException("Не существует конфеты с таким номером");
+            }
+
+            _sweets.RemoveAt(sweetId);
+        }
+
+
+    }
+}
