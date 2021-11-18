4c9f162af9fc555637a27ef15cc536ae73da5a5f
KirillKanabay <kirill.kanabay@gmail.com>
2021-11-18 16:11:25 +0300

    Удаление валидации из свойств в классе Lolipop

--- NewYearGift/Models/Lollipop.cs
+++ NewYearGift/Models/Lollipop.cs
@@ -7,24 +7,10 @@ namespace NewYearGift.Models
     /// </summary>
     public class Lollipop:Sweet
     {
-        private string _flavor;
-
         /// <summary>
         /// Вкус 
         /// </summary>
-        public string Flavor
-        {
-            get => _flavor;
-            set
-            {
-                if (string.IsNullOrWhiteSpace(value))
-                {
-                    throw new ArgumentException("Вкус леденца не может быть пустым.");
-                }
-
-                _flavor = value;
-            }
-        }
+        public string Flavor { get; set; }
         public Lollipop(string name, string manufacturer, double weight, double sugarWeight, decimal price, string flavor) : base(name, manufacturer, weight, sugarWeight, price)
         {
             Flavor = flavor;
