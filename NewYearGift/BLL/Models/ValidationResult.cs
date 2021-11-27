92f6a621f35113cc4596e9e794d169f5048c35e5
KirillKanabay <kirill.kanabay@gmail.com>
2021-11-27 22:31:29 +0300

    Рефакторинг валидации

---
+++ NewYearGift/BLL/Models/ValidationResult.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace NewYearGift.BLL.Models
+{
+    public class ValidationResult
+    {
+        public IEnumerable<string> Errors { get; set; }
+        public string Error { get; set; }
+        public bool HasError { get; set; }
+        public bool IsSuccess { get; set; }
+    }
+}
